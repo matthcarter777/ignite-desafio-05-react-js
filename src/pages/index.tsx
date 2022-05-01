4be1cb99995ec16e6a06a077bb89a663a4179c92
Mateus <mateushbsb@gmail.com>
2022-05-01 17:18:36 -0300

    IGNITEDES - 002: create post list

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,11 @@
 /* eslint-disable @typescript-eslint/explicit-module-boundary-types */
 /* eslint-disable @typescript-eslint/explicit-function-return-type */
 import { GetStaticProps } from 'next';
+import Link from 'next/link';
+import { format } from 'date-fns';
+import ptBR from 'date-fns/locale/pt-BR';
 
+import { AiOutlineCalendar, AiOutlineUser } from 'react-icons/ai';
 import { getPrismicClient } from '../services/prismic';
 
 import commonStyles from '../styles/common.module.scss';
@@ -26,14 +30,63 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-export default function Home() {
-  // TODO
-  return <h1>Hellow world</h1>;
+export default function Home({ postsPagination }: HomeProps) {
+  return (
+    <main className={styles.container}>
+      <div className={styles.posts}>
+        {postsPagination?.results.map(post => (
+          <Link key={post.uid} href={`/post/${post.uid}`}>
+            <a>
+              <strong>{post.data.title}</strong>
+              <p>{post.data.subtitle}</p>
+              <div className={styles.info}>
+                <time>
+                  <AiOutlineCalendar />
+                  <span>{post.first_publication_date}</span>
+                </time>
+                <span>
+                  <AiOutlineUser />
+                  <span>{post.data.author}</span>
+                </span>
+              </div>
+            </a>
+          </Link>
+        ))}
+      </div>
+    </main>
+  );
 }
 
-// export const getStaticProps = async () => {
-//   // const prismic = getPrismicClient({});
-//   // const postsResponse = await prismic.getByType(TODO);
+export const getStaticProps = async () => {
+  const prismic = getPrismicClient({});
+  const postsResponse = await prismic.getByType('posts', {
+    pageSize: 2,
+  });
 
-//   // TODO
-// };
+  const posts = postsResponse.results.map(post => {
+    return {
+      uid: post.uid,
+      first_publication_date: format(
+        new Date(post.first_publication_date),
+        'ee MMM yyyy',
+        {
+          locale: ptBR,
+        }
+      ),
+      data: {
+        title: post.data.title,
+        subtitle: post.data.subtitle,
+        author: post.data.author,
+      },
+    };
+  });
+
+  return {
+    props: {
+      postsPagination: {
+        results: posts,
+        next_page: postsResponse.next_page,
+      },
+    },
+  };
+};
