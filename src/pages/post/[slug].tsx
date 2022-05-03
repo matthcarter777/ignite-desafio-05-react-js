c266fb4abc0d8173c9912ed07e28ba1d78d624cf
Mateus Henrique <mateushbsb@gmail.com>
2022-05-03 16:40:05 -0300

    IGNITEDES - 008: render isFallbeck message

--- src/pages/post/[slug].tsx
+++ src/pages/post/[slug].tsx
@@ -1,3 +1,5 @@
+/* eslint-disable no-return-assign */
+/* eslint-disable no-param-reassign */
 /* eslint-disable react/no-danger */
 /* eslint-disable @typescript-eslint/explicit-module-boundary-types */
 /* eslint-disable @typescript-eslint/explicit-function-return-type */
@@ -5,9 +7,10 @@ import { GetStaticPaths, GetStaticProps } from 'next';
 import { RichText } from 'prismic-dom';
 import { AiOutlineCalendar, AiOutlineUser } from 'react-icons/ai';
 import { BiTime } from 'react-icons/bi';
-import { format } from 'date-fns';
+import { format, parseISO } from 'date-fns';
 import ptBR from 'date-fns/locale/pt-BR';
 
+import { useRouter } from 'next/router';
 import { getPrismicClient } from '../../services/prismic';
 
 import commonStyles from '../../styles/common.module.scss';
@@ -35,6 +38,35 @@ interface PostProps {
 }
 
 export default function Post({ post }: PostProps) {
+  console.log(post);
+
+  const router = useRouter();
+
+  console.log(post);
+
+  if (router.isFallback) {
+    return <h2>Carregando...</h2>;
+  }
+
+  const formattedDate = format(
+    parseISO(post.first_publication_date),
+    'dd MMM yyyy',
+    {
+      locale: ptBR,
+    }
+  );
+
+  const totalWords = post.data.content.reduce((total, contentItem) => {
+    total += contentItem.heading.split(' ').length;
+
+    const words = contentItem.body.map(item => item.text.split(' ').length);
+    words.map(word => (total += word));
+
+    return total;
+  }, 0);
+
+  const averageReading = `${Math.ceil(totalWords / 200).toString()} min`;
+
   return (
     <main className={commonStyles.container}>
       <article className={styles.post}>
@@ -45,7 +77,7 @@ export default function Post({ post }: PostProps) {
         <div className={commonStyles.info}>
           <time>
             <AiOutlineCalendar />
-            <span>{post.first_publication_date}</span>
+            <span>{formattedDate}</span>
           </time>
           <span>
             <AiOutlineUser />
@@ -53,7 +85,7 @@ export default function Post({ post }: PostProps) {
           </span>
           <span>
             <BiTime />
-            <span>{post.data.author}</span>
+            <span>{averageReading}</span>
           </span>
         </div>
         {post.data.content.map(({ heading, body }) => (
@@ -71,9 +103,23 @@ export default function Post({ post }: PostProps) {
 }
 
 export const getStaticPaths: GetStaticPaths = async () => {
+  const prismic = getPrismicClient({});
+
+  const posts = await prismic.getByType('posts', {
+    pageSize: 2,
+  });
+
+  const paths = posts.results.map(post => {
+    return {
+      params: {
+        slug: post.uid,
+      },
+    };
+  });
+
   return {
-    paths: [],
-    fallback: 'blocking',
+    paths,
+    fallback: true,
   };
 };
 
@@ -84,20 +130,19 @@ export const getStaticProps: GetStaticProps = async ({ params }) => {
   const response = await prismic.getByUID('posts', String(slug));
 
   const post = {
-    first_publication_date: format(
-      new Date(response.first_publication_date),
-      'ee MMM yyyy',
-      {
-        locale: ptBR,
-      }
-    ),
+    first_publication_date: response.first_publication_date,
     data: {
       title: response.data.title,
       banner: {
         url: response.data.banner.url,
       },
       author: response.data.author,
-      content: response.data.content,
+      content: response.data.content.map(content => {
+        return {
+          heading: content.heading,
+          body: content.body,
+        };
+      }),
     },
   };
 
@@ -105,6 +150,5 @@ export const getStaticProps: GetStaticProps = async ({ params }) => {
     props: {
       post,
     },
-    revalidate: 60 * 60,
   };
 };
